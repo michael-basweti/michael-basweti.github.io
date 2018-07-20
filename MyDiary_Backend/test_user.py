08efd7af340c9f71f3709b3509b12c66e64cc304
michael-basweti <baswetima@gmail.com>
2018-07-21 00:12:56 +0300

    add travis

--- MyDiary_Backend/test_user.py
+++ MyDiary_Backend/test_user.py
@@ -1,31 +1,50 @@
-import unittest
-import json
-import os
-import pytest
-from flask import Flask
+"""
+nose tests for the api
+"""
 from nose.tools import assert_true
-from manage import app
-from flask_testing import TestCase
 import requests
 
 
-
 def test_get_all():
+    """
+    test all returns
+    :return:
+    """
     response = requests.get('http://127.0.0.1:5000/mydiary/api/v1.0/entries/get')
     assert_true(response.ok)
 
+
 def test_post():
+    """
+    test post
+    :return:
+    """
     response = requests.post('http://127.0.0.1:5000/mydiary/api/v1.0/entries/post')
     assert_true(response.ok)
 
+
 def test_get_one():
+    """
+    test get one
+    :return:
+    """
     response = requests.get('http://127.0.0.1:5000/mydiary/api/v1.0/entries/get/1')
     assert_true(response.ok)
 
+
 def test_edit_one():
+    """
+    test editing
+    :return:
+    """
     response = requests.put('http://127.0.0.1:5000/mydiary/api/v1.0/entries/edit/1')
     assert_true(response.ok)
 
+
 def test_delete_one():
+    """
+    test delete
+    :return:
+    """
     response = requests.delete('http://127.0.0.1:5000/mydiary/api/v1.0/entries/delete/1')
     assert_true(response.ok)
