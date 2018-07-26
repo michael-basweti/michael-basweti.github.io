657c2eba8ad868826fc7a6d0fee794aafc50de71
Michael Alex Basweti <baswetima@gmail.com>
2018-07-27 00:10:22 +0300

    Update app.py

--- FlaskApi/app.py
+++ FlaskApi/app.py
@@ -8,8 +8,7 @@ from apis import api
 app = Flask(__name__)  # pylint: disable=invalid-name
 api.init_app(app)
 app.config['SECRET_KEY'] = 'thisismysecretkeynigga'
-conn = psycopg2.connect(database="mydiary", user="postgres",  # pylint: disable=invalid-name
-                        password="trizabas2017", host="127.0.0.1", port="5432")
+
 
 print('opened databse successfully')
 app.run(debug=True)
