a9a6a3e4171fe646c19d52b368c8eda11f19b0fb
Michael Alex Basweti <baswetima@gmail.com>
2018-07-30 21:38:10 +0300

    Update user.py

--- FlaskApi/apis/user.py
+++ FlaskApi/apis/user.py
@@ -12,8 +12,8 @@ app = Flask(__name__)  # pylint: disable=invalid-name
 app.config['SECRET_KEY'] = 'thisismysecretkeynigga'
 
 api = Namespace('User', description='operations that can be performed on the user')
-conn = psycopg2.connect(database="mydiary", user="postgres", password="trizabas2017",
-                        host="127.0.0.1", port="5432")
+conn = psycopg2.connect(database="df4keijp3j4gsn", user="cslzrgdwqphsdg", password="1f370b6b19a69f6aab5cb73d09fc1edfc4a7eee53680458d01ed498c777b1932",
+                        host="ec2-23-23-242-163.compute-1.amazonaws.com", port="5432")
 
 user_model = api.model('User', {  # pylint: disable=invalid-name
     'username': fields.String(required=True, description='Username'),
